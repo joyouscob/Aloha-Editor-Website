20a363f2f3a36615bebdc89a5c21f0901fffb565
Johannes Schüth <j.schueth@gentics.com>
2012-01-25 11:12:03 +0100

    Updated download link to new version of aloha

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ new TWTR.Widget({
 <br />
 </div>
 
-                                <a title="Download Aloha HTML5 WYSIWYG Editor" href="https://github.com/downloads/alohaeditor/Aloha-Editor/alohaeditor-0.20.2.zip" class="button"><span class="text">Download Aloha Editor</span><span class="version">Version 0.20.2</span></a>
+                                <a title="Download Aloha HTML5 WYSIWYG Editor" href="https://github.com/downloads/alohaeditor/Aloha-Editor/alohaeditor-0.20.3.zip" class="button"><span class="text">Download Aloha Editor</span><span class="version">Version 0.20.3</span></a>
 
 				<a title="Aloha HTML5 WYSIWYG Editor license" id="license" href="/license.php">License</a><div title="Aloha HTML5 WYSIWYG Editor languages" id="language">Available in en, fr, de <a href="/join_us.php#translator">and more</a></div>
 
