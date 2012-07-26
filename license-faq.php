18b52f205b521e45e3f025fe188acb09a1e7674e
Rene Kapusta <rene@evo42.net>
2012-07-26 18:34:32 +0200

    updated license faq

--- license-faq.php
+++ license-faq.php
@@ -9,77 +9,70 @@
 		
 			<div id="teaser" class="container_20" style="padding-top:30px">
 				<div class="grid_20">
-					<h2>Aloha Editor GPLv2 License FAQ</h2>
-					<p>The following questions and answers are our personal interpretation and are not legally approved.</p>
-					<h3>In short terms</h3>
+                    <h2>Aloha Editor GPL License FAQ</h2>
+                    <p>The following questions and answers are our personal interpretation and are not legally approved.</p>
+                    <h3>In short terms</h3>
 
-						<h4>You are free to</h4>
-						<ul>
-							<li>include Aloha Editor in GPLv2 or later software and ship as one package </li>
-							<li>include Aloha Editor in MIT, BSD or similar licensed software and ship as one package with a FOSS license exception</li>
-							<li>include Aloha Editor in commercial software and ship as one package</li>
-							<li>Use or sell services with software, where Aloha Editor is legally included</li>
-						</ul>
+                            <h4>You are free to</h4>
+                            <ul>
+                                    <li>include Aloha Editor in GPLv2 or late licensed software and ship as one package </li>
+                                    <li>include Aloha Editor in MIT, BSD or similar licensed software and ship as one package with a FOSS license exception</li>
+                            </ul>
+
+                            <h4>You need to publish</h4>
+                            <ul>
+                                    <li>the license to users</li>
+                                    <li>derived work to the users as "source code" (eg. as link in the comment of the site)</li>
+                            </ul>
+
+                            <h4>You may buy a commercial license (to support the community)</h4>
+                            <ul>
+                                    <li>to support continuous development of Aloha Editor</li>
+                                    <li>if you want to sell a commercial software and don't want to publish derived work</li>
+                                    <li>if you want to provide any service using Aloha Editor and don't want to publish derived work</li>
+                            </ul>
+
+                            <h4>Our interpretation of following GPLv2 or later terms regarding Javascript code</h4>
+                            <ul>
+                                    <li>"source code" are all single files as in the github repository.</li>
+                                    <li>"object code" are minified, combined or any with any other method modified "source code" files</li>
+                                    <li>"derived work" changes to the source code (except configs) of Aloha Editor</li>
+                            </ul>
+
 					
-						<h4>You need to publish</h4>
-						<ul>
-							<li>the license to users</li>
-							<li>changes to Aloha Editor to community as "source code" (eg. download in legal notices)</li>
-							<li>derived work from Aloha Editor to users as "source code" (eg. download in legal notices)</li>
-						</ul>
-					
-						<h4>You may buy a commercial license (to support the community)</h4>
-						<ul>
-							<li>to support continuous development of Aloha Editor</li>
-							<li>if you want to sell a commercial software and don't want to publish its source code</li>
-							<li>if you want to provide any service using Aloha Editor and don't want to publish its source code</li>
-							<li>if you prefer to use a non-GPL user interface framework for Aloha Editor (jQuery UI)</li>
-						</ul>
-					
-						<h4>Our interpretation of following GPLv2 terms regarding Javascript code</h4>
-						<ul>
-							<li>"source code" are all single files as in the github repository.</li>
-							<li>"object code" are minified, combined or any with any other method modified "source code" files</li>
-							<li>"derived work" are all plugins or extensions  connected with Aloha Editor using the Aloha Editor API or other methods</li>
-						</ul>
-					
-					<h3>Frequently asked questions and answers</h3>
-					
-					<h4 id=1>1. I have server side software such as a CMS (or other software) GPLv3 licensed. Can I CHANGE OR EXTEND AND DISTRIBUTE Aloha Editor with it?</h4>
-					<p>
-					Yes, you can and we encourage you to do so. AGPLv3 and GPLv3 are compatible. See <a href="http://www.gnu.org/licenses/gpl-faq.html#v3Notwithstanding">http://www.gnu.org/licenses/gpl-faq.html#v3Notwithstanding</a>. If you modify the Aloha Editor source code or distribute derived work you need to publish the changes or the source code of the derived work AGPLv3 compatible.
-					</p>
+                            <h3>Frequently asked questions and answers</h3>
 
-					<h4 id=2>2. I have server side software such as a CMS (or other software) BSD (revised), MIT, ASL or similar licensed. Can I CHANGE OR EXTEND AND DISTRIBUTE Aloha Editor with it?</h4>
-					<p>
-					Yes, you can and we encourage you to do so. You are not allowed to change the license of Aloha Editor components. If you modify the source code or distribute derived work you need to publish the changes or the source code of the derived work AGPLv3 compatible. You need a FOSS license exception to distribute Aloha Editor with BSD, MIT or similar licensed software. Eg. Locomotive CMS is MIT, Aloha Editor is AGPLv3, with the FOSS exception, Locomotive CMS can distribute Aloha Editor and remain MIT, but Aloha Editor will remain AGPLv3. <a href="http://aloha-editor.org/contact.php">Contact us to get a FOSS license exception!</a>
-					</p>
+                            <h4 id="1">1. Aloha Editor is licensed "GPLv2 or later"? What does that mean?</h4>
+                            <p>
+                            It means that users will always be permitted to use it, and even change it, according to the terms of GPLv2 - even after later versions of the GPL (like GPLv3) are available. (for more detail see <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0-faq.html#VersionTwoOrLater" data-bitly-type="bitly_hover_card">GNU FAQ</a>)
+                            </p>
 
-					<h4 id=3>3. Do I need to publish modifications to a GPLv3 server side component?</h4>
-					<p>
-					No, you do not need to publish those changes. The GPL does not require you to release your modified version, or any part of it. You are free to make modifications and use them privately, without ever releasing them. This applies to organizations (including companies), too; an organization can make a modified version and use it internally without ever releasing it outside the organization. But if you release the modified version to the public in some way, the GPL requires you to make the modified source code available to the program's users, under the GPL. Publishing it to the Aloha Editor project is welcomed. See <a href="http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic">http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic</a>.
-					</p>
+                            <h4 id="2">2. I have server side software such as a CMS (or other software) GPLv3 licensed. Can I CHANGE OR EXTEND AND DISTRIBUTE Aloha Editor with it?</h4>
+                            <p>
+                            Yes, you can and we encourage you to do so. Aloha Editor is licensed under GPLv2 and later. That means later versions of the GPL (like GPLv3) are also valid.
+                            </p>
 
-					<h4 id=4>4. Do I need to publish extensions to a GPLv3 server side component?</h4>
-					<p>
-					No, you do not need to publish extensions as not needed by the GPLv3 from your server side software as long as you do not modify the software. If you modify it you may need to publish the changes depending on the GPLv3. See <a href="http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic">http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic</a>
-					</p>
+                            <h4 id="3">3. I have server side software such as a CMS (or other software) BSD (revised), MIT, ASL or similar licensed. Can I CHANGE OR EXTEND AND DISTRIBUTE Aloha Editor with it?</h4>
+                            <p>
+                            Yes, you are allowed to to distribute Aloha Editor with BSD, MIT or similar licensed software after getting a FOSS license exception from us. You are not allowed to change the license of Aloha Editor itself. If you distribute derived work you need to publish the changes or the source code of the derived work GPLv2 or later compatible.  Eg. Locomotive CMS is MIT, Aloha Editor is GPLv2 or later. With the FOSS exception, Locomotive CMS can distribute Aloha Editor and remain MIT, but Aloha Editor reamains GPLv2 or later. <a href="http://aloha-editor.org/contact.php" data-bitly-type="bitly_hover_card">Contact us to get a FOSS license exception!</a>
+                            </p>
 
-					<h4 id=5>5. I want to use a GPLv3 licensed software, which is distributed as one package with Aloha Editor and provide a SAAS Service. Can I do that?</h4>
-					<p>
-					Yes, you can and we encourage you to do so. If you want to change the source code you need to publish changes to your users. See <a href="http://www.gnu.org/licenses/gpl-faq.html#AGPLv3CorrespondingSource">http://www.gnu.org/licenses/gpl-faq.html#AGPLv3CorrespondingSource</a> or <a href="http://www.gnu.org/licenses/gpl-faq.html#WhatDoesCompatMean">http://www.gnu.org/licenses/gpl-faq.html#WhatDoesCompatMean</a>. Publishing it to the Aloha Editor project is welcomed. If you don't want to publish any change think about a commercial license and support with it the continuous development of Aloha Editor.
-					</p>
+                            <h4 id="4">4. Do I need to publish modifications to a GPL server side component in every case?</h4>
+                            <p>
+                            No, the GPL does not require you to release your modified version, or any part of it in every case. You are free to make modifications and use them privately, without ever releasing them. This applies to organizations (including companies), too; an organization can make a modified version and use it internally without ever releasing it outside the organization. But if you release the modified version to the <strong>public</strong> in some way, the GPL requires you to make the modified source code available to the program's users, under the GPL. Publishing it to the Aloha Editor project is welcomed. See <a href="http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic" data-bitly-type="bitly_hover_card">http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic</a>.
+                            </p>
 
-					<h4 id=6>6. I want to distribute Aloha Editor for a fee. Can I do that?</h4>
-					<p>
-					Yes, you can and we encourage you to do so. This is part of free software. See <a href="http://www.gnu.org/philosophy/selling.html">http://www.gnu.org/philosophy/selling.html</a>.
-					</p>
-<!--
-					<h4 id=7>7. I want to sell Aloha Editor along with my commercial and non open-source CMS (project) and I don't want apply to the AGPLv3. Or I just don't worry about that. Can I do that?</h4>
-					<p>
-					Yes, you can distribute Aloha Editor with your commercial project. <a href="contact.php">Buy a commercial license</a> (to support the Aloha Editor community).
-					</p>
--->
+                            <h4 id="5">5. Do I need to publish extensions to a GPL server side component?</h4>
+                            <p>
+                            No, you do not need to publish extensions as not needed by the GPL from your server side software as long as you do not modify the software. If you modify it you may need to publish the changes depending on the GPL. See <a href="http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic" data-bitly-type="bitly_hover_card">http://www.gnu.org/licenses/gpl-faq.html#GPLRequireSourcePostedPublic</a>
+                            </p>
+
+
+                            <h4 id="6">6. I want to distribute Aloha Editor for a fee. Can I do that?</h4>
+                            <p>
+                            Yes, you can and we encourage you to do so. This is part of free software. See <a href="http://www.gnu.org/philosophy/selling.html" data-bitly-type="bitly_hover_card">http://www.gnu.org/philosophy/selling.html</a>.
+                            </p>
+
 				</div>
 				<div class="clear"></div>
 			</div>
--- license.php
+++ license.php
@@ -10,7 +10,7 @@
 		<div id="teaser" class="container_20">
 			<div class="grid_20">
 				<h1>Aloha Editor License</h1>
-				<p>Aloha Editor is meant to be of benefit to both free open source as well as commercial software developers. It should be able to sustain continous development by itself and thus help all stakeholders profiting from the developments to come. Therefore we adopted a dual licensing approach for Aloha Editor.</p>
+                <p>Aloha Editor is meant to be of benefit to both free open source as well as commercial software developers. It should be able to sustain continous development by itself and thus help all stakeholders profiting from the developments to come. Therefore we adopted a dual licensing approach for Aloha Editor.</p>
 <p><a href="license-faq.php">Read the license FAQ for license adoption</a></p>
 			</div>
 			<div class="clear"></div>
@@ -20,23 +20,33 @@
 					<h3>Open Source License.</h3>
 					<p><img alt="" src="http://www.opensource.org/files/garland_logo.png" style="float:left; padding-right:20px;"/>We want to 
 					keep this work open to all now and in future. The Open Source version of this great software is released under the terms of  
-					<a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPLv2</a> or later. General Public Licenses are intended 
-					to guarantee your freedom to share and change all versions of a program--to make sure it remains free software for all its users.
-					You may be allowed to redistribute Aloha Editor along with other opensource projects that are MIT, BSD or similar licensed with 
-					the FOSS license exception. Eg. <a href="http://locomotivecms.com/">Locomotive CMS</a> is MIT, Aloha Editor is GPLv2 or later, with the FOSS exception, Locomotive CMS can distribute Aloha Editor and remain MIT, but Aloha Editor will remain GPLv2 or later. <a href="<?=$path?>contact.php">Contact us to get a FOSS license exception</a>!
+					<a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPLv2</a> or later license.
+					
+					
+					That means you are allowed to use the terms of later versions of the GPL (like <a href="http://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GPLv3</a>) as well.
+                    Generally Public Licenses are intended to guarantee your freedom to share and change all versions of a program ensuring
+                    its existence as free software for all its users. With a FOSS license exception you may be allowed to redistribute Aloha Editor along with other
+                    open source projects that are MIT, BSD or similar licensed.
+                    For example: <a href="http://locomotivecms.com/">Locomotive CMS</a> is MIT,
+                    Aloha Editor is GPLv2, with the FOSS exception, Locomotive CMS can distribute Aloha Editor and remain MIT, but Aloha Editor will remain GPLv2.
+                    <a href="<?=$path?>contact.php">Contact us to get a FOSS license exception</a>!
 					</p>
 					<h3>Language files License.</h3>
 					<p><img alt="" src="http://i.creativecommons.org/l/by/3.0/88x31.png" style="float:left; padding-right:20px;"/> All language translations
-					distributed along with the project Aloha Editor you can distribute, remix, tweak, and build upon your work, even commercially, 
-					as long as they credit the creator for the original creation. All contributed translations are contributed under the terms of 
-					<a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons Attribution License</a>.
+                    distributed along with the project Aloha Editor you can distribute, remix, tweak, and build upon your work, even commercially,
+                    as long as they credit the creator for the original creation. All contributed translations are contributed under the terms of
+                    <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank" >Creative Commons Attribution License</a>.
+
 					</p>
 					<h3>Commercial License.</h3>
+					
 					<p>
-						The commercial version of Aloha Editor allows to use, modify, copy and distribute without
-						any restrictions as stated in the commercial license agreement. 
-					<a href="<?=$path?>contact.php">Contact us for more information</a>!
-					</p>
+                    If you are a commercial software developer and you want to release your software under a different license than GPLv2 or later licenses,
+                    you may purchase commercial licenses from Gentics Software GmbH. By purchasing commercial licenses, Gentics offers you the commercial
+                    distribution of Aloha Editor, which gives you the freedom to choose different ways of licensing and get professional support from
+                    Aloha Editor developers.
+                    <br>
+                    <a href="<?=$path?>contact.php">Contact us for more information</a>!
 					
 				</div>
 				<div class="clear"></div>
