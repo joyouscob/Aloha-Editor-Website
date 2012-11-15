0c3e69a679b5c72081efefb77213a55378e677dc
Johannes Schüth <j.schueth@gentics.com>
2012-11-15 12:42:21 +0100

    Added some stuff that tobias steiner added on purpose

---
+++ demos/apa/aloha-editor/plugins/common/link/nls/i18n.js
@@ -0,0 +1,25 @@
+define({
+	"root":  {
+		"button.addlink.tooltip": "Insert Link",
+		"button.removelink.tooltip": "Remove Link",
+		"newlink.defaulttext": "New Link",
+		"padding.decrease": "Decrease padding",
+		"padding.increase": "Increase padding",
+		"floatingmenu.tab.link": "Link",
+		"link.target.self": "Self",
+		"link.target.blank": "Blank",
+		"link.target.parent": "Parent",
+		"link.target.top": "Top",
+		"link.target.framename": "Framename",
+		"link.target.legend": "Target",
+		"link.title.legend": "Title",
+		"insertLink": "ctrl+k"
+	},
+		"ca": true,
+		"de": true,
+		"mk": true,
+		"pt-br": true,
+		"ru": true,
+		"uk": true,
+		"zh-hans": true
+});
