bfdca95b07807ffbd2f70ca6b7faca70d0e92be5
Rene Kapusta <rene@evo42.net>
2012-11-29 10:44:55 +0100

    added supported browsers page

---
+++ supported-browsers.php
@@ -0,0 +1,38 @@
+<? include('inc/header.inc'); ?>
+
+</head>
+
+<body>
+	<div id="wrapper" class="container_20">
+
+<? include('inc/menu.inc'); ?>
+		
+		<div id="teaser" class="container_20">
+			<div class="grid_20">
+				<h1>Supported Browser Version for Aloha Editor</h1>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div id="teaser2" class="container_20" style="padding-top:30px">
+				<div class="grid_20">
+	                <p>Aloha Editor is currently supporting the following browsers:</p>
+					<ul>
+						<li>Internet Explorer 8, 9</li>
+						<li>Mozilla Firefox 15, 16, 17</li>
+						<li>Google Chrome 21, 22, 23</li>
+						<li>Safari 5, 6</li>
+					</ul>
+					<br />
+					<p class="smallinfo">We are reserving the right to cancel support for a certain browser versions at any given time without prior notice.</p>
+					<br />
+					<p class="smallinfo">November 2012</p>
+				</div>
+				<div class="clear"></div>
+			</div>
+
+<? include('inc/footer.inc'); ?>
+
+	</div>
+
+</body>
+</html>
