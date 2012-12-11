7cfd34fcdb9f525981f99b87cb69ecd05c63f92f
Rene Kapusta <rene@evo42.net>
2012-12-11 17:04:02 +0100

    added missing file; added @RobertLeePlummerJr as new contributor

---
+++ howto-store-data-with-aloha-editor-ajax.php
@@ -0,0 +1,30 @@
+<? include('inc/header.inc'); ?>
+
+</head>
+
+<body>
+	<div id="wrapper" class="container_20">
+
+<? include('inc/menu.inc'); ?>
+		
+		<div id="teaser" class="container_20">
+			<div class="grid_20">
+				<h1>Store data from editables with Aloha Editor</h1>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div id="teaser2" class="container_20" style="padding-top:30px">
+				<div class="grid_20">
+	                <p>Save content to your backend with Aloha Editor and AJAX</p>
+					
+					<script src="https://gist.github.com/1448270.js"> </script>
+				</div>
+				<div class="clear"></div>
+			</div>
+
+<? include('inc/footer.inc'); ?>
+
+	</div>
+
+</body>
+</html>
